56676cf78093e7fffc6a20680f1f37b97aecbc54
hee.youn <heesung6701@gmail.com>
2024-10-14 23:57:55 +0900

    remove useless log

--- src/bot.py
+++ src/bot.py
@@ -10,7 +10,6 @@ from dotenv import load_dotenv
 
 class DiscordBot:
     TAG = "DiscordBot"
-    command_dict = None
 
     def __init__(self, csv_url, token, on_ready_bot: Optional[Callable] = None):
         self.loop = None
@@ -21,41 +20,38 @@ class DiscordBot:
         self.bot = bot
         self.csv_url = csv_url
         self.token = token
+        self.command_dict = self.get_command_dict()
 
         @bot.event
         async def on_ready():
-            self.command_dict = await self.get_command_dict()
-            await sync_dynamic_commands(self.command_dict)
+            await sync_dynamic_commands()
 
             print(f'[{self.TAG}] Logged in as {bot.user}')
 
             if on_ready_bot:
                 on_ready_bot()
 
-        async def sync_dynamic_commands(command_dict: dict):
-            print(f'[{self.TAG}] sync_dynamic_commands {command_dict}')
-            for name, message in command_dict.items():
+        async def sync_dynamic_commands():
+            for name, message in self.command_dict.items():
                 if name.isdigit():
                     continue
-                await add_dynamic_slash_command(name, name, message)
+                add_dynamic_slash_command(name, name, message)
 
             print(f'[{self.TAG}] tree.sync start')
             try:
                 synced = await self.bot.tree.sync()
-                print(synced)
-                print(f"[{self.TAG}] Synced {len(synced)} commands.")
+                print(f"[{self.TAG}] Synced {len(synced)} commands. | ${synced}")
             except Exception as e:
                 print(f"[{self.TAG}] Error syncing commands: {e}")
 
-        async def add_dynamic_slash_command(name, description, message):
-            print(f'[{self.TAG}] add_dynamic_slash_command {name} {description}')
+        def add_dynamic_slash_command(name, description, message):
 
             @bot.tree.command(name=name, description=description)
             async def hybrid_command(interaction: discord.Interaction):
                 print(f'[{self.TAG}] {name} executed {message}')
                 await interaction.response.send_message(f"> {message}")
 
-    async def get_command_dict(self) -> dict[str, str]:
+    def get_command_dict(self) -> dict[str, str]:
         try:
             print(f"[{self.TAG}] read_csv")
             df = pd.read_csv(self.csv_url)
